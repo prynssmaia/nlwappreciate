3660a6508daef20a2774def84baf658d8b532910
Prynss <prynss_lp@hotmail.com>
2021-06-20 22:18:38 -0300

    environment configuration

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+
+app.get("/", (req,res) => {
+    return res.send("Hello NLW NODE")
+})
+
+app.post("/post", (req,res) => {
+    return res.send("hello nlw post")
+})
+
+
+app.listen(4576, () => console.log("Server is running at localhost:4576"))
