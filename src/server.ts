19e90fd7c71a21546e4090872481a836caca3d11
Prynss <prynss_lp@hotmail.com>
2021-06-21 21:15:10 -0300

    create orm database configuration

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
+import "reflect-metada"
 import express from "express"
+
+import "./database"
 
 const app = express()
 
@@ -9,6 +12,5 @@ app.get("/", (req,res) => {
 app.post("/post", (req,res) => {
     return res.send("hello nlw post")
 })
-
 
 app.listen(4576, () => console.log("Server is running at localhost:4576"))
